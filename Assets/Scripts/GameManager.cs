74104a06fb19f59bd5bc4d8ca61b25fbce095df4
rtq9731 <rtq9731@gmail.com>
2020-07-15 01:08:39 +0900

    플레이어 점프 완성 및 이동 일부분 구현 -> 경계구역 만들어야함

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public UnityEngine.Vector2 limitMin = UnityEngine.Vector2.zero;
+    public UnityEngine.Vector2 limitMax = UnityEngine.Vector2.zero;
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,25 @@ using DG.Tweening;
 public class Player : MonoBehaviour
 {
 
-    Vector3 currentPositon = Vector3.zero;
+    public UnityEngine.Vector2 limitMin = UnityEngine.Vector2.zero;
+    public UnityEngine.Vector2 limitMax = UnityEngine.Vector2.zero;
+
+    Vector2 currentPositon = Vector3.zero;
     Rigidbody2D rigid = null;
     float timer = 0f;
     bool canJump = true;
+
+    Vector2 currentPos = Vector3.zero;
+    float currentPosX = 0;
+    float currentPosY = 0;
 
     [SerializeField]
     float jumpPower = 0;
     [SerializeField]
     float jumpCoolTime = 0;
+    [SerializeField]
+    float moveTime = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +36,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        currentPos = transform.localPosition;
         timer += Time.deltaTime;
         if (timer >= jumpCoolTime)
             canJump = true;
@@ -33,12 +44,28 @@ public class Player : MonoBehaviour
         {
             StartCoroutine(jump());
         }
-        
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.DOMoveX(currentPos.x + 1f, moveTime).SetEase(Ease.OutQuad);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.DOMoveX(currentPos.x - 1f, moveTime);
+        }
+
+        currentPosX = Mathf.Clamp(currentPos.x, limitMin.x, limitMax.x);
+
+        currentPosX = Mathf.Clamp(currentPos.x, limitMin.x, limitMax.x);
+
+        currentPos = new Vector2(currentPosX, currentPosY);
+        transform.localPosition = currentPos;
     }
 
     IEnumerator jump()
     {
-        transform.DOLocalJump(new Vector3(0f, 0f, 0f), jumpPower, 1, 0.2f);
+        transform.DOLocalJump(new Vector3(currentPos.x, 0f, 0f), jumpPower, 1, 0.5f);
         timer = 0f;
         canJump = false;
         yield return 0;
