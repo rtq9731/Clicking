08fb7f041ab42bfb00da5ff10e428686d99975d4
rtq9731 <rtq9731@gmail.com>
2020-08-01 11:21:17 +0900

    필요없는 에셋 삭제 및 배경 씬에 넣어놓음

---
+++ Assets/Scripts/BackgroundMove.cs
@@ -0,0 +1,28 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class BackgroundMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    float moveTime = 0;
+
+    float time = Time.deltaTime;
+    Transform myPotision = null;
+
+    void Start()
+    {
+        myPotision = GetComponent<Transform>();
+        transform.DOMoveY(myPotision.x)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
