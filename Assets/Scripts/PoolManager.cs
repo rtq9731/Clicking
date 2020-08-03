f901155e2c9ddda6097fe97fc9225e2e025ec2a3
rtq9731 <rtq9731@gmail.com>
2020-08-03 23:37:47 +0900

    노트 위치 다 만듬 / 코드 작성 준비 완료

--- Assets/Scripts/PoolManager.cs
+++ Assets/Scripts/PoolManager.cs
@@ -2,15 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PoolManager : MonoBehaviour
 {
-
-    int a = 0;
-
     // Start is called before the first frame update
     void Start()
     {
-       a = Random.Range(1, 16);
+        
     }
 
     // Update is called once per frame
