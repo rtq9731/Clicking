baab428f0970bab4706566581b54da0a227f8926
HiJinie <choiheejin5959@gmail.com>
2020-08-06 07:45:24 +0900

    Start, Main connect

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Main : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Click()
+    {
+        SceneManager.LoadScene (1);
+    }
+}
---
+++ Assets/Scripts/Start.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Start : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Click()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
